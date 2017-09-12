366e07eb7747fefdd513cb570d497201aee24460
Neo <binario0011@hotmail.com>
2017-09-12 14:38:53 -0300

    Start Config API

---
+++ src/Providers/GmailServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+namespace NiltonMeloX\GmailApiEasy\Gmail\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use NiltonMeloX\GmailApiEasy\Gmail;
+
+class GmailServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/gmail.php' => config_path('gmail.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/gmail.php', 'gmail'
+        );
+
+//        $config = $this->app['config']->get('gmail');
+//        $this->app->instance('gmail', new Gmail($config));
+    }
+
+    public function provides()
+    {
+        return ['gmail'];
+    }
+
+
+}
